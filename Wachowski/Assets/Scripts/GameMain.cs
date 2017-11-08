9811b70bd043ea55e7188287f642f1eb93e2591d
Zhitao Zhao <31802he@163.com>
2017-11-07 22:44:35 -0700

    add GameMain

---
+++ Wachowski/Assets/Scripts/GameMain.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour {
+
+    public static GameMain Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
