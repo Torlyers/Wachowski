88562c74107d1d2a37b7673de0f10c0f6b5d4028
Zhitao <31802he@163.com>
2017-11-09 11:19:40 -0700

    basic control

--- Wachowski/Assets/Scripts/Player.cs
+++ Wachowski/Assets/Scripts/Player.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public GameObject player;
+    public float speed;
+    private Rigidbody pRb;
+    public float force;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start ()
+    {
+        pRb = player.GetComponent<Rigidbody>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        player.transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space");
+            pRb.AddForce(transform.up * force);
+        }
+    }
 }
---
+++ Wachowski/Assets/Scripts/Shadow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour {
+
+    public GameObject shadow;
+    public float speed;
+    private Rigidbody pRb;
+    public float force;
+
+    // Use this for initialization
+    void Start()
+    {
+        pRb = shadow.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shadow.transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("A");
+            pRb.AddForce(transform.forward * force);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        pRb.AddForce(-transform.forward * 9.81f);
+
+        //var grav = new Vector3(0, 0, 9.81f);
+        //var pos = shadow.transform.position;
+        //pos += grav * Time.fixedDeltaTime;
+        //shadow.transform.position = pos;
+    }
+}
