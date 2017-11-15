4dfde1086c9270f2653eb2293365cd79dbe041c2
u1148646 <utkarshrao.viswavasu@utah.edu>
2017-11-15 11:29:18 -0700

    title screen buttons work and added transition to endgame. need to add endgame scene

--- Wachowski/Assets/Scripts/Player.cs
+++ Wachowski/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour {
 
@@ -65,7 +66,7 @@ public class Player : MonoBehaviour {
 
     public void dead()
     {
-
+        SceneManager.LoadScene("EndGame");
     }
 
     private void OnCollisionEnter(Collision collision)
