f05b5a3c1d84799824665e2d20d24a35a1b258fa
u1148646 <utkarshrao.viswavasu@utah.edu>
2017-11-14 20:01:29 -0700

    added some moving obstacles

--- Wachowski/Assets/Scripts/SineObstacleMovement.cs
+++ Wachowski/Assets/Scripts/SineObstacleMovement.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class SineObstacleMovement : MonoBehaviour {
 
-    public GameObject[] obstacleList = new GameObject[5];
-    private Vector3[] obstaclePosition = new Vector3[5];
-    
+    public GameObject[] obstacleList1 = new GameObject[5];
+    public GameObject[] obstacleList2 = new GameObject[2];
+
+    private Vector3[] obstaclePosition1 = new Vector3[5];
+    private Vector3[] obstaclePosition2 = new Vector3[2];
+
     // Use this for initialization
-	void Start () {
+    void Start () {
 		for(int i = 0; i < 5; i++)
         {
-            obstaclePosition[i] = obstacleList[i].transform.position;
+            obstaclePosition1[i] = obstacleList1[i].transform.position;
         }
-	}
+        for (int i = 0; i < 2; i++)
+        {
+            obstaclePosition2[i] = obstacleList2[i].transform.position;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,7 +28,11 @@ public class SineObstacleMovement : MonoBehaviour {
 
         for(int i = 0; i<5;i++)
         {
-            obstacleList[i].transform.position = obstaclePosition[i] + new Vector3(0f, 0f, Mathf.Sin(Time.time * 5));
+            obstacleList1[i].transform.position = obstaclePosition1[i] + new Vector3(Mathf.Sin(Time.time * 5), 0f, 0f );
         }
-	}
+        for(int i = 0; i < 2; i++)
+        {
+            obstacleList2[i].transform.position = obstaclePosition2[i] + new Vector3(0f, 0f, Mathf.Sin(Time.time * 2) * 2);
+        }
+    }
 }
