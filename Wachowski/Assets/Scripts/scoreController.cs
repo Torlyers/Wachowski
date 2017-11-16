5a85aa893fdfc80e36da1b07bcd48ca42549784f
u1148646 <utkarshrao.viswavasu@utah.edu>
2017-11-15 19:38:54 -0700

    added start scene, end scene and score text, moved obstacles to the right so we can demo level and edited jump

---
+++ Wachowski/Assets/Scripts/scoreController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreController : MonoBehaviour {
+
+    public Text scoreText;
+    private int score;
+    // Use this for initialization
+	void Start () {
+        score = 0;
+        scoreText.text = "Score: " + score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        score += 2;
+        scoreText.text = "Score: " + score;
+	}
+}
